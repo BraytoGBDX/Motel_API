71c22c07db7f848e9f3d22c9551561b6cf1c81ff
des-arrosho <arroyo210504@gmail.com>
2023-11-30 20:34:46 -0600

    Se modifico el archivo startSeed.js

--- src/lib/seeders/startSeed.js
+++ src/lib/seeders/startSeed.js
@@ -1,26 +1,53 @@
-import WorkerCode from "../../models/workerCode";
-import workerCodesData from './workerCodeSeed.js'
+import {exit} from 'node:process'
+import rooms from '../../models/rooms.js'
+import Rooms from './roomsSeed.js'
+import db from '../../config/db.js'
 
-const seedWorkerCodes = async () => {
-    try {
-      // Sincroniza el modelo con la base de datos
-      await WorkerCode.sync({ force: true });
-  
-      // Inserta los datos de ejemplo
-      await WorkerCode.bulkCreate(workerCodesData);
-  
-      console.log('Datos de WorkerCode insertados correctamente.');
-    } catch (error) {
-      console.error('Error al insertar datos de WorkerCode:', error);
-    } finally {
-      // Cierra la conexión con la base de datos si es necesario
-      await WorkerCode.sequelize.close();
+
+
+const importData = async () => {
+    try{
+        //Autenticar
+        await db.authenticate()
+        //Generar columnas
+        await db.sync()
+        //Importar los datos
+        await Promise.all([ Rooms.bulkCreate(rooms)])
+        console.log(`Se han importado los datos de las tablas catalogo de manera correcta`);
+        exit
+    }catch(err){
+        console.log(err)
+        exit(1);
     }
-  };
-  
 
-  if(process.argv[2]==="-i"){
-    seedWorkerCodes();
 }
-  // Llama a la función para ejecutar el seeder
-  
+
+if(process.argv[2] === "-i"){
+    importData();
+}
+
+const deleteData = async () => {
+    try{
+        const queryResetRooms = "ALTER TABLE tbc_categories AUTO_INCREMENT = 1;"     
+        await Promise.all([Rooms.destroy({
+            where:{},
+            truncate:false
+        })
+       ])
+
+        await Promise.all([
+            db.query(queryResetRooms,{
+            raw:true
+            
+        })
+      ])
+    }catch(err){
+        console.log(err)
+            exit(1);
+    }
+}
+
+if(process.argv[2] === "-d"){
+    deleteData();
+}
+//0 node, 1 seeder, 2 argumento
