57ba7b4fd4595567dcf0edaca7327d595853b9aa
des-arrosho <arroyo210504@gmail.com>
2023-11-30 20:23:15 -0600

    Modificación del archivo rooms.js

--- src/models/rooms.js
+++ src/models/rooms.js
@@ -1,1 +1,26 @@
-console.log("prueba");
+import { DataType, DataTypes } from "sequelize"
+import db from '../config/db.js'
+
+const Rooms = db.define('tbc_rooms',{
+    
+    typeRoom: { //* tipo de habitación
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+    description: { //* Descripcion
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    image:{ //* Imagen
+        type: DataTypes.STRING(150),
+        allowNull: false,
+        defaultValue: "Por definir"
+    },
+    price: {    //* precio de habitacones 
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue:0   
+    }
+})
+
+export default Rooms
